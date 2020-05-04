5d420b8c0ba33b2cce11c90aec7ebeda79c36a6e
Victor <madivcb@gmail.com>
2020-05-04 19:46:11 -0300

    created middleware to validate request id

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -19,7 +19,18 @@ function logRequests(req, res, next) {
   console.timeEnd(logLabel);
 }
 
+function validateProjectId(req, res, next) {
+  const { id } = req.params;
+
+  if (!isUuid(id)) {
+    return res.status(400).json({ error: 'Invalid project ID.' });
+  }
+
+  next();
+}
+
 app.use(logRequests);
+app.use('/projects/:id', validateProjectId);
 
 app.get('/projects', (req, res) => {
   const { title } = req.query;
